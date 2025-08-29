fae2e5beda0b8cd306af8052b480e9999ab82d94
manoahLinks <manoahluka@gmail.com>
2025-08-29 23:51:33 +0100

    feat: integrate deposit and withdrawal

---
+++ frontend/src/constants/yao_process.ts
@@ -0,0 +1,4 @@
+    const VAULT = "M_Vemv863rkJUYUIf-LrnCCZfawTizFzfNH00rdGom0";
+    const AO_TOKEN = "0syT13r0s0tgPmIed95bJnuSqaD29HQNN8D3ElLSrsc";
+
+    export { VAULT, AO_TOKEN };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,19 @@ import App from "./App.tsx";
 import { WalletProvider } from "./components/WalletProvider.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { ChatbotProvider } from "./contexts/ChatbotContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a React Query client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <WalletProvider>
         <ChatbotProvider config={{ position: "bottom-right", theme: "auto" }}>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </ChatbotProvider>
       </WalletProvider>
     </ThemeProvider>
